be7304628fb5fc74109e92bbc34cbaba09ca9185
jjames <jjames@newrelic.com>
2022-11-06 21:49:24 -0800

    Bytes to string

---
+++ src/java/examples/BytesToString.java
@@ -0,0 +1,10 @@
+package examples;
+
+public class BytesToString {
+    public static void main(String[] args) {
+        //bytes to string
+        byte[] byteArr = new byte[] {87, 79, 87, 46, 46, 46};
+        String value = new String(byteArr);
+        System.out.println(value);
+    }
+}
